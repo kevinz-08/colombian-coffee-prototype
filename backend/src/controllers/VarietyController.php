ef391f5ab820b3c58fd2cf094ed3f45ee5f5d3e2
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-08-01 12:58:37 +0000

    build: :art: Change the routes and add some news models

---
+++ backend/src/controllers/VarietyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Variety;
+require_once _DIR_ . '/../Models/Variety.php';
+
+class VarietyController
+{
+    public function index() {
+        $variety = new Variety();
+        $data = $variety->getAll();
+        echo json_encode($data);
+    }
+
+    public function show($id) {
+        $variety = new Variety();
+        $data = $variety->getById($id);
+        echo json_encode($data);
+    }
+
+    public function store() {
+        $input = json_decode(file_get_contents('php://input'), true);
+        $variety = new Variety();
+        $success = $variety->create($input);
+        echo json_encode(['success' => $success]);
+    }
+
+    public function update($id) {
+        $input = json_decode(file_get_contents('php://input'), true);
+        $variety = new Variety();
+        $success = $variety->update($id, $input);
+        echo json_encode(['success' => $success]);
+    }
+
+    public function destroy($id) {
+        $variety = new Variety();
+        $success = $variety->delete($id);
+        echo json_encode(['success' => $success]);
+    }
+}
